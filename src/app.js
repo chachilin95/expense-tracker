efd99a81ab73911ee6cbe1e1469c34a866628ae9
Alejandro Figueroa <tester@Alejandros-MBP.home>
2019-06-29 16:13:06 -0500

    End of Video 79

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -40,20 +40,35 @@ const Help = () => {
 const NotFound = () => {
     return (
         <div>
-            404!
+            404 - <Link to='/'>Go Home</Link>
         </div>
+    );
+}
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Expensify</h1>
+            <NavLink exact to='/' activeClassName="is-active">Dashboard</NavLink>
+            <NavLink exact to='/create' activeClassName="is-active">Create Expense</NavLink>
+            <NavLink exact to='/edit' activeClassName="is-active">Edit Expense</NavLink>
+            <NavLink exact to='/help' activeClassName="is-active">Help</NavLink>
+        </header>
     );
 }
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route exact path='/' component={ExpenseDashboard}/>
-            <Route exact path='/create' component={AddExpense}/>
-            <Route exact path='/edit' component={EditExpense}/>
-            <Route exact path='/help' component={Help}/>
-            <Route component={NotFound}/>
-        </Switch>
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path='/' component={ExpenseDashboard}/>
+                <Route exact path='/create' component={AddExpense}/>
+                <Route exact path='/edit' component={EditExpense}/>
+                <Route exact path='/help' component={Help}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
