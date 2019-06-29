5fd2a7cb2202c2576ddb9bffee4e92a01f17fc1e
Alejandro Figueroa <tester@Alejandros-MBP.home>
2019-06-29 15:49:31 -0500

    End of Video 78

--- src/app.js
+++ src/app.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate app</p>, document.getElementById('app'));
+const ExpenseDashboard = () => {
+    return (
+        <div>
+            This is the Expense Dashboard
+        </div>
+    );
+};
+
+const AddExpense = () => {
+    return (
+        <div>
+            This is the Add Expense Page
+        </div>
+    );
+};
+
+const EditExpense = () => {
+    return (
+        <div>
+            This is the Edit Expense Page
+        </div>
+    );
+};
+
+const Help = () => {
+    return (
+        <div>
+            This is the Help Page
+        </div>
+    );
+};
+
+const NotFound = () => {
+    return (
+        <div>
+            404!
+        </div>
+    );
+}
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path='/' component={ExpenseDashboard}/>
+            <Route exact path='/create' component={AddExpense}/>
+            <Route exact path='/edit' component={EditExpense}/>
+            <Route exact path='/help' component={Help}/>
+            <Route component={NotFound}/>
+        </Switch>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
