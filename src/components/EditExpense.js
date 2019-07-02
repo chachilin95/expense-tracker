e8f11c708f016e2a5bfe1f5bece49772fdd066c9
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-02 00:04:37 -0500

    End of Video 108

--- src/components/EditExpense.js
+++ src/components/EditExpense.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
-export default class EditExpense extends React.Component {
-    
-    constructor(props) {
-        super(props);
+const EditExpense = (props) => (
+    <div>
+        <ExpenseForm 
+            expense={props.expense}
+            onSubmit={(expense) => {
+                props.dispatch(editExpense(props.expense.id, expense));
+                props.history.push('/');
+            }}/>
+        <button onClick={() => {
+            props.dispatch(removeExpense({ id: props.expense.id }));
+            props.history.push('/');
+        }}>Remove</button>
+    </div>
+);
 
-        console.log(props);
-    }
+const mapStateToProps = (state, props) => {
+    return {
+        expense: state.expenses.find((expense) => (expense.id === props.match.params.id))
+    };
+};
 
-    render() {
-        return (
-            <div>
-                Editing the expense with id of {this.props.match.params.id}
-            </div>
-        );
-    }
-}
+export default connect(mapStateToProps)(EditExpense);
