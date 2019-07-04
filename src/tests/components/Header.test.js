a763177d44df2986875fab46c18f3a02f7ac6a7b
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-04 00:39:31 -0500

    End of Video 118

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+
+import Header from '../../components/Header';
+
+test('Should render Header', () => {
+    const renderer = new ReactShallowRenderer();
+    renderer.render(<Header/>);
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
