910a3ca365f042acfe8cfe62d1b5fd9a36948e50
Alejandro Figueroa <tester@Alejandros-MacBook-Pro.local>
2019-07-08 22:04:31 -0500

    End of Video 133

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                loader: 'babel-loader',
-                test: /\.js$/, // matches all *.js files
-                exclude: /node_modules/ //do this to prevent trying to build everything inside of this
-            },
-            {
-                test: /\.s?css$/, // matches all *.css & *.scss files
-                use: [ // allows us to use multiple loaders
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        mode: 'development',
+        module: {
+            rules: [
+                {
+                    loader: 'babel-loader',
+                    test: /\.js$/, // matches all *.js files
+                    exclude: /node_modules/ //do this to prevent trying to build everything inside of this
+                },
+                {
+                    test: /\.s?css$/, // matches all *.css & *.scss files
+                    use: [ // allows us to use multiple loaders
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
 };
