a1d91e1bb2e223b0b0e172d16cf07b5a41374699
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-08 21:44:45 -0500

    End of Video 127

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import moment from 'moment';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 import { filters, altFilters } from '../fixtures/filters';
 
@@ -29,4 +30,48 @@ test('Should render ExpenseListFilters with alt data correctly', () => {
         filters: altFilters 
     });
     expect(wrapper).toMatchSnapshot();
+});
+
+test('Should handle text change', () => {
+    const value = 'testing...';
+    wrapper.find('input').simulate('change', {
+        target: { value }
+    });
+    expect(setTextFilterSpy).toHaveBeenLastCalledWith(value);
+});
+
+test('Should sort by date', () => {
+    const value = 'date';
+    wrapper.setProps({
+        filters: altFilters
+    });
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByDateSpy).toBeCalled();
+});
+
+test('Should sort by amount change', () => {
+    const value = 'amount';
+    wrapper.find('select').simulate('change', {
+        target: { value }
+    });
+    expect(sortByAmountSpy).toBeCalled();
+});
+
+test('Should handle date changes', () => {
+    const dates = { 
+        startDate: moment(0).add(4, 'years'), 
+        endDate: moment(0).add(8, 'years')
+    };
+    wrapper.find('withStyles(DateRangePicker)').prop('onDatesChange')(dates);
+    expect(setStartDateSpy).toHaveBeenLastCalledWith(dates.startDate);
+    expect(setEndDateSpy).toHaveBeenLastCalledWith(dates.endDate);    
+});
+
+test('Should handle date focus changes', () => {
+    const calendarFocused = 'endDate';
+
+    wrapper.find('withStyles(DateRangePicker)').prop('onFocusChange')(calendarFocused);
+    expect(wrapper.state('calendarFocused')).toBe(calendarFocused);
 });
