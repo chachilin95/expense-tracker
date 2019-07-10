5e4085bfb270359d86827479d5d0e925575bf85f
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-09 19:54:54 -0500

    End of Video 138

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export default ({ id, description, amount, createdAt }) => (
     <div>
@@ -7,6 +9,10 @@ export default ({ id, description, amount, createdAt }) => (
             to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>        
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')} 
+             -  
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>  
 );
