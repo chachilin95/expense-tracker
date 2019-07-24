f04e9614e947e599ea4e04547ef0573ebb33e3df
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-24 18:53:30 -0500

    Created PrivateRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory as createHistory } from 'history';
+
+import PrivateRoute from './PrivateRoute';
 
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import Login from '../components/Login';
-import Header from '../components/Header';
 import NotFound from '../components/NotFound';
 
 export const history = createHistory();
@@ -14,12 +15,11 @@ export const history = createHistory();
 const AppRouter = () => (
     <Router history={history}>
         <div>
-            <Header/>
             <Switch>
                 <Route exact path='/' component={Login}/>
-                <Route exact path='/dashboard' component={ExpenseDashboard}/>
-                <Route exact path='/create' component={AddExpense}/>
-                <Route exact path='/edit/:id' component={EditExpense}/>
+                <PrivateRoute exact path='/dashboard' component={ExpenseDashboard}/>
+                <PrivateRoute exact path='/create' component={AddExpense}/>
+                <PrivateRoute exact path='/edit/:id' component={EditExpense}/>
                 <Route component={NotFound}/>
             </Switch>
         </div>
