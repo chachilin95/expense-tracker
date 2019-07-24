2f0f361a36c0ad207b974bd3270b4796bfd8e082
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-24 18:58:14 -0500

    Created PublicRoute

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory as createHistory } from 'history';
 
+import PublicRoute from './PublicRoute';
 import PrivateRoute from './PrivateRoute';
 
 import AddExpense from '../components/AddExpense';
@@ -16,7 +17,7 @@ const AppRouter = () => (
     <Router history={history}>
         <div>
             <Switch>
-                <Route exact path='/' component={Login}/>
+                <PublicRoute exact path='/' component={Login}/>
                 <PrivateRoute exact path='/dashboard' component={ExpenseDashboard}/>
                 <PrivateRoute exact path='/create' component={AddExpense}/>
                 <PrivateRoute exact path='/edit/:id' component={EditExpense}/>
