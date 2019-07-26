02f407bdd490ef83327496a09c413c6e8808ac67
Alejandro Figueroa <chachilin95@knights.ucf.edu>
2019-07-25 19:22:17 -0500

    Finished Login and began Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Expensify</h1>
-        <NavLink exact to='/' activeClassName="is-active">Dashboard</NavLink>
-        <NavLink exact to='/create' activeClassName="is-active">Create Expense</NavLink>
-        <button onClick={startLogout}>Logout</button>
+    <header className='header'>
+        <div className='content-container'>
+            <div className='header__content'>
+                <Link className='header__title' to='/'>
+                    <h1>Expensify</h1>
+                </Link>
+                <button onClick={startLogout}>Logout</button>
+            </div>
+        </div>
     </header>
 );
 
