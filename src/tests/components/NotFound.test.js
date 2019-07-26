9af8abf40eb85772886ac67206ac445b45bf05f0
Alejandro Figueroa <tester@Alejandros-MacBook-Pro.local>
2019-07-25 23:23:12 -0500

    Created test for Loading page, fixed NotFound test

--- src/tests/components/NotFound.test.js
+++ src/tests/components/NotFound.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NotFound from '../../components/NotFound';
 
-test('Should render NotFound with expense', ()=>{
+test('Should render NotFound page', ()=>{
     const wrapper = shallow(<NotFound/>)
     expect(wrapper).toMatchSnapshot();
 })
