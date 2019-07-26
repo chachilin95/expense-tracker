bab76de61731d73458a87052ae71c2176b989873
Alejandro Figueroa <tester@Alejandros-MacBook-Pro.local>
2019-07-25 22:37:54 -0500

    Styled Add and Edit pages

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,14 +6,13 @@ export default class ExpenseForm extends React.Component {
 
     constructor(props) {
         super(props);
-        
+
         this.state = {
             description: props.expense ? props.expense.description : '',
             note: props.expense ? props.expense.note : '',
             amount: props.expense ? (props.expense.amount / 100).toString() : '',
-            createdAt: props.expense ? moment(props.expense.createdAt): moment(),
+            createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calendarFocused: false,
-            editMode: this.props.inEditMode,
             error: ''
         };
     }
@@ -65,40 +64,40 @@ export default class ExpenseForm extends React.Component {
 
     render() {
         return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.onSubmit}>
-                    <input 
-                        className='text-input'
-                        type='text'
-                        placeholder='Description'
-                        autoFocus
-                        value={this.state.description}
-                        onChange={this.onDescriptionChange}
-                        name='description'/>
-                    <input 
-                        className='text-input'
-                        type='number'
-                        placeholder='Amount'
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                        name='amount'/>
-                    <SingleDatePicker
-                        date={this.state.createdAt}
-                        onDateChange={this.onDateChange}
-                        focused={this.state.calendarFocused}
-                        onFocusChange={this.onFocusChange}
-                        numberOfMonths={1}
-                        isOutsideRange={() => false}/>
-                    <textarea
-                        className='textarea'
-                        placeholder='Add a note... (optional)'
-                        value={this.state.note}
-                        onChange={this.onNoteChange}
-                        name='note'/>
-                    <button>{this.props.inEditMode ? "Edit" : "Add"} Expense</button>
-                </form>                
-            </div>
+            <form className='form' onSubmit={this.onSubmit}>
+                {this.state.error && <p className='form__error'>{this.state.error}</p>}
+                <input
+                    className='text-input'
+                    type='text'
+                    placeholder='Description'
+                    autoFocus
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                    name='description' />
+                <input
+                    className='text-input'
+                    type='number'
+                    placeholder='Amount'
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                    name='amount' />
+                <SingleDatePicker
+                    date={this.state.createdAt}
+                    onDateChange={this.onDateChange}
+                    focused={this.state.calendarFocused}
+                    onFocusChange={this.onFocusChange}
+                    numberOfMonths={1}
+                    isOutsideRange={() => false} />
+                <textarea
+                    className='textarea'
+                    placeholder='Add a note... (optional)'
+                    value={this.state.note}
+                    onChange={this.onNoteChange}
+                    name='note' />
+                <div>
+                    <button className='button'>Save Expense</button>
+                </div>
+            </form>
         );
     }
 }
